256d70d2e2fce2135c7439cf4fea4c533fe621fe
Brian Shea <bs@Totoro.local>
2017-09-27 17:39:00 -0400

    stuff

--- src/js/checklist.js
+++ src/js/checklist.js
@@ -1,1 +1,14 @@
 // Checklist.js
+function chkCheck()
+{
+if(
+    document.getElementById("chkCoupler").Checked &
+    document.getElementById("chkTirePressure").Checked &
+    document.getElementById("chkElectrical").Checked &
+    document.getElementById("chkChains").Checked &
+    document.getElementById("chkJack").Checked)
+    {
+        document.getElementById("lblReadyToGo").style.display = "block";
+    }
+
+}
