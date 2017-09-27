0498a239c6cabebef870dea4854db481a73f3abd
Brian Shea <bs@Totoro.local>
2017-09-27 17:45:40 -0400

    ready to go message

--- src/js/checklist.js
+++ src/js/checklist.js
@@ -2,11 +2,11 @@
 function chkCheck()
 {
 if(
-    document.getElementById("chkCoupler").Checked &
-    document.getElementById("chkTirePressure").Checked &
-    document.getElementById("chkElectrical").Checked &
-    document.getElementById("chkChains").Checked &
-    document.getElementById("chkJack").Checked)
+    document.getElementById("chkCoupler").checked &
+    document.getElementById("chkTirePressure").checked &
+    document.getElementById("chkElectrical").checked &
+    document.getElementById("chkChains").checked &
+    document.getElementById("chkJacks").checked)
     {
         document.getElementById("lblReadyToGo").style.display = "block";
     }
