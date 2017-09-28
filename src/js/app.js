6bbefe1a3ed66a91ffafcc7ffffb402952b3a031
Brian Shea <bs@Totoro.local>
2017-09-28 15:38:32 -0400

    first part of trailer light check

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,39 @@
-// app.js
+//--------SWITCH BETWEEN TABS-----------------
+
+function showHome()
+{
+    document.getElementById("tabHome").style.display = "block";
+    document.getElementById("tabBackup").style.display = "none";
+    document.getElementById("tabChecklist").style.display = "none";
+}
+
+function showBackup()
+{
+    document.getElementById("tabHome").style.display = "none";
+    document.getElementById("tabBackup").style.display = "block";
+    document.getElementById("tabChecklist").style.display = "none";
+}
+
+function showChecklist()
+{
+    document.getElementById("tabHome").style.display = "none";
+    document.getElementById("tabBackup").style.display = "none";
+    document.getElementById("tabChecklist").style.display = "block";
+}
+
+//-----------ERROR LIGHTS--------------
+
+function toggleAlertLight(isError, alertName)
+{
+    console.log("-alert light update-");
+    if(Boolean(isError))
+    {
+        console.log("Setting error");
+        document.getElementById("alert-" + alertName).src = "images/x-" + alertName + ".png";
+    }
+    else
+    {
+        console.log("Setting OK");
+        document.getElementById("alert-" + alertName).src = "images/ok-" + alertName + ".png";
+    }
+}
